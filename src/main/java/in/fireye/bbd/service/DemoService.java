a2a9c21862ca2b3134a2caf26c21994794fef907
bysunexus <1669371+bysunexus@users.noreply.github.com>
2025-04-15 15:36:25 +0800

    refactor(bbd): 修改 Ollama chat 方法使用设置工具回调- 注释掉 .toolCallbacks(ToolCallbacks.from(dateTimeTools)) 行 - 添加注释解释原因：Ollama 在使用 tool calling时暂不支持 stream

--- src/main/java/in/fireye/bbd/service/DemoService.java
+++ src/main/java/in/fireye/bbd/service/DemoService.java
@@ -6,7 +6,6 @@ import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.model.tool.ToolCallingChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.tool.ToolCallbacks;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -17,8 +16,9 @@ public class DemoService {
   private final DateTimeTools dateTimeTools;
 
   public Flux<String> chat(String msg) {
+    // ollama 在使用tool calling的时候不支持stream
     ChatOptions chatOptions = ToolCallingChatOptions.builder()
-      .toolCallbacks(ToolCallbacks.from(dateTimeTools))
+//      .toolCallbacks(ToolCallbacks.from(dateTimeTools))
       .build();
     return ollamaChatModel
       .stream(new Prompt(msg, chatOptions))
